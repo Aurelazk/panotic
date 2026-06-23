cadf76e180fd429266f0052ce590605acb6f0873
rayann1405 <rayannbachabi0@gmail.com>
2026-06-23 13:17:46 +0100

    fix: update COLORS import path in AdsScreen

--- AANID/frontend/src/screens/Placeholder/AdsScreen.tsx
+++ AANID/frontend/src/screens/Placeholder/AdsScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { COLORS } from '../../../rayann/frontend/src/constants/colors';
+import { COLORS } from '../../constants/theme';
 
 export default function AdsScreen() {
   return (
